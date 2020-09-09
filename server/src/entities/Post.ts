e9e1d263f120490511c5986844c9fba70bdf82c9
Quang Tran <54891109+quangtran2209@users.noreply.github.com>
2020-09-10 01:12:24 +0700

    Introduce MikroORM TypeGraphQL CRUD Actions

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -1,18 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { ObjectType, Field, Int } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
-
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
-  @Property({ type: 'date' })
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ type: 'date', onUpdate: () => new Date() })
+  @Field(() => String)
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property({ type: 'text' })
+  @Field()
+  @Property({ type: "text" })
   title!: string;
-
 }
