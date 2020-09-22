d31b4f6a9de78c1e90612876ec7aeff3bfee4618
Quang Tran <54891109+quangtran2209@users.noreply.github.com>
2020-09-23 00:40:57 +0700

    Created register page using Formik form

---
+++ web/src/pages/register.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Box,
+  Button,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Input your username"
+              label="Username"
+            ></InputField>
+
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              ></InputField>
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              variantColor="teal"
+              isLoading={isSubmitting}
+            >
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
